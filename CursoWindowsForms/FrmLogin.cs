354d0fe3b9ec3846c6a248db1e44eea1567ebbfa
00 <00@A0005>
2024-04-03 22:07:12 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/FrmLogin.cs
@@ -0,0 +1,53 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btEntrar_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            bool situacaoLogin = login.VerificarLogin(txtNome.Text, txtSenha.Text);
+            if(situacaoLogin == true)
+            {
+                FrmPrincipal frm = new FrmPrincipal();
+                frm.Show();
+                this.Hide();
+            }
+            else 
+            {
+                MessageBox.Show("Nome ou senha incorreta!");
+            }
+        }
+
+        private void txtNome_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                txtSenha.Focus();
+            }
+        }
+
+        private void txtSenha_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+               btEntrar.Focus();
+            }
+        }
+    }
+}
---
+++ CursoWindowsFormsBiblioteca/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca
+{
+    public class Login
+    {
+        public bool VerificarLogin(string nome, string senha)
+        {
+            if (nome.Trim() == "RHAEL" && senha.Trim() == "123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
